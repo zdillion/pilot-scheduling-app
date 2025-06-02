c59c830925a264cb9baf7b5e710d155015b23b35
zdillion <zdillion3@comcast.net>
2025-06-02 17:07:42 -0400

    Delete lib directory

--- lib/db.ts
+++
@@ -1,7 +0,0 @@
-import { neon } from "@neondatabase/serverless"
-
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL is not set")
-}
-
-export const sql = neon(process.env.DATABASE_URL)
